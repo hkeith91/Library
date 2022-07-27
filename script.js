41fbd8e6344f1c45771c601365d6853c3038a763
Hector <hector.keith91@gmail.com>
2022-07-27 18:40:27 -0400

    created myLibrary array and populated with example books.  Created table with basic styles in html

--- script.js
+++ script.js
@@ -1,14 +1,36 @@
+const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, "not yet read.");
+const prideAndPredjudice = new Book("Pride and Prejudice", "Jane Austen", 432, "not yet read.");
+const crimeAndPunishment = new Book("Crime and Punishment", "Fyodor Dostoyevski", 573, "not yet read.");
+const salemsLot = new Book("Salem's Lot", "Stephan King", 325, "Read");
+const cloudAtlas = new Book("Cloud Atlas", "David Mitchell", 453, "Read");
+
+let myLibrary = [
+    theHobbit,
+    prideAndPredjudice,
+    crimeAndPunishment,
+    salemsLot,
+    cloudAtlas
+];
+
+function addBookToLibray(){
+
+}
+
+function displayBooks(){
+
+}
+
 function Book(title, author, pages, isRead){
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.isRead = isRead;
     this.info = function(){
-        console.log(`${title} by ${author}, ${pages} pages, ${isRead}`);
+        let bookInfo = `${title} by ${author}, ${pages} pages, ${isRead}`;
+        return bookInfo;
     }
 
-    const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, "not yet read.")
-
-    console.log(theHobbit.info());
-
 }
+
+
+console.log(theHobbit.info());
